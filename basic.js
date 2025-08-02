4e30576ce389f2f678b79ffe6f2522f21dc5f456
Darknight <philipamezuweh@gmail.com>
2025-08-02 18:39:15 +0000

    elevator pitch added

--- basic.js
+++ basic.js
@@ -2,5 +2,13 @@
     function toggleMenu() {
       var navLinks = document.getElementById("navLinks");
       navLinks.classList.toggle("active");
-    }
+      const toggle = document.querySelector('.menu-toggle');
+      // toggle.classList.toggle("hide");
+// const navLinks = document.querySelector('.nav-links');
+// const toggle = document.querySelector('.menu-toggle');
+// toggle.addEventListener('click', () => {
+//   navLinks.classList.toggle('active');
+//   toggle.classList.toggle('hide');
+// });
+     }
  
